88e36e88e61feb49d9963e5d103080449137c531
MNisarAli <dr.nisaralig@gmail.com>
2023-10-16 22:48:56 +0500

    Setup letter opner config for email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,12 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Use letter_opener for development
+  config.action_mailer.delivery_method = :letter_opener
+
+  # Perform deliveries in development mode
+  config.action_mailer.perform_deliveries = true
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
