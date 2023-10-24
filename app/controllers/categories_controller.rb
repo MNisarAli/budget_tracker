425e51602da994577006201afa910dbd1ae35469
MNisarAli <dr.nisaralig@gmail.com>
2023-10-24 14:29:31 +0500

    Add action to create a new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,29 @@
 class CategoriesController < ApplicationController
+  def index
+    @user = current_user
+    @categories = @user.categories.order(created_at: :desc)
+    @expenses = current_user.expenses
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+
+    if @category.save
+      flash[:notice] = 'New Category Created Successfully'
+      redirect_to user_categories_path
+    else
+      flash[:notice] = 'Please provide a valid Icon image and Name'
+      redirect_to new_user_category_path
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
